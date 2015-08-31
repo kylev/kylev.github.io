3b5ee05792a86ab5bb549df0ecd2002d3506bb43
Kyle VanderBeek <kylev@gametime.co>
2015-08-30 20:38:24 -0700

    First build attempt with pdfkit, which seems a bit better.

--- lines/lines.js
+++ lines/lines.js
@@ -1,17 +1,35 @@
-var doc = new jsPDF();
+var PDFDocument = require('pdfkit'),
+    blobStream = require('blob-stream');
+
+var doc = new PDFDocument();
 
 function updatePdf(arg) {
   var paper = $('#lines [name=paper]').val();
   var orientation = $('#lines [name=orientation]').val();
 
-  var newDoc = new jsPDF(orientation, 'in', paper);
+  //var newDoc = new jsPDF(orientation, 'pt', paper);
+  var newDoc = new PDFDocument();
+  var stream = newDoc.pipe(blobStream());
+  stream.on('finish', function () {
+    var pdfString = stream.toBlobURL('application/pdf');
+    $('.preview-pane').attr('src', pdfString);
+  });
 
-  //$('preview-pane').
+  var marginAbove;
+  var ascender = 3/8;
+  var xHeight = 1/4;
+  var descender = 3/8;
+  var totalHeight = marginAbove + ascender + xHeight + descender;
 
-  doc = newDoc;
+  newDoc.moveTo(100, 100)
+    .lineTo(200, 100)
+    .stroke();
 
-  var pdfString = doc.output('datauristring');
-  $('.preview-pane').attr('src', pdfString);
+  //newDoc.line(2,2,-2,2);
+
+  console.log(newDoc);
+
+  doc = newDoc.end();
 
   return false;
 }
@@ -19,3 +37,7 @@ function updatePdf(arg) {
 function savePdf() {
   doc.save();
 }
+
+window.updatePdf = updatePdf;
+window.savePdf = savePdf;
+//module.exports.updatePdf = updatePdf;
