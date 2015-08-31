67725f7f91a2bbfa567a8fa2b58e5d34401db2b6
Kyle VanderBeek <kylev@gametime.co>
2015-08-30 21:48:55 -0700

    Basic, ugly version now works with horizontal lines.

--- lines/lines.js
+++ lines/lines.js
@@ -3,29 +3,51 @@ var PDFDocument = require('pdfkit'),
 
 var doc = new PDFDocument();
 
+var colors = ['green', 'black', 'black', 'red'];
+
 function updatePdf(arg) {
   var paper = $('#lines [name=paper]').val();
   var orientation = $('#lines [name=orientation]').val();
 
-  //var newDoc = new jsPDF(orientation, 'pt', paper);
-  var newDoc = new PDFDocument();
+
+  var newDoc = new PDFDocument({
+    size: paper,
+    layout: orientation
+  });
+
   var stream = newDoc.pipe(blobStream());
   stream.on('finish', function () {
     var pdfString = stream.toBlobURL('application/pdf');
     $('.preview-pane').attr('src', pdfString);
   });
 
-  var marginAbove;
-  var ascender = 3/8;
-  var xHeight = 1/4;
-  var descender = 3/8;
+  console.log("New doc is high: " + newDoc.page.height);
+
+  var marginAbove = 20;
+  // TODO this is currently 3:2:3 for Copperplate
+  var xHeight = 18;
+  var ascender = xHeight / 2 * 3;
+  var descender = xHeight / 2 * 3;
   var totalHeight = marginAbove + ascender + xHeight + descender;
 
-  newDoc.moveTo(100, 100)
-    .lineTo(200, 100)
-    .stroke();
+  var repeatCount = Math.floor(newDoc.page.height / totalHeight);
+  var leftPoint = 20;
+  var rightPoint = newDoc.page.width - leftPoint;
 
-  //newDoc.line(2,2,-2,2);
+  //repeatCount = 1;
+  //console.log("repeat " + repeatCount + "left" + leftPoint + 'right' + rightPoint);
+
+  for (var i = 0, offset = 0; i < repeatCount; i++, offset += totalHeight) {
+    [marginAbove, ascender, xHeight, descender].reduce(function (base, additional, idx) {
+      var lineAt = base + additional;
+      console.log('blah ' + base + 'add ' + additional, " idx" + idx);
+      newDoc.moveTo(leftPoint, lineAt)
+        .lineTo(rightPoint, lineAt)
+        .stroke(colors[idx]);
+
+      return lineAt;
+    }, offset);
+  }
 
   console.log(newDoc);
 
