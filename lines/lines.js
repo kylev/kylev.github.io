428483ade4067241887f6326a6cd4c01e9b0e569
Kyle VanderBeek <kylev@gametime.co>
2015-08-31 11:55:15 -0700

    Make ratios dynamic; add optional x-marker.

--- lines/lines.js
+++ lines/lines.js
@@ -5,10 +5,19 @@ var doc = new PDFDocument().end();
 
 var colors = ['green', 'black', 'black', 'red'];
 
+var ratios = {
+  x1: function (x) { return x; },
+  x2: function (x) { return x * 2; },
+  x3: function (x) { return x * 3; },
+  r32: function (x) { return x / 2 * 3; }
+};
+
 function updatePdf() {
   var paper = $('#lines [name=paper]').val();
   var orientation = $('#lines [name=orientation]').val();
   var xHeight = $('#lines [name=xHeight]').val() * 1;
+  var adRatio = $('#lines [name=adHeight]').val();
+  var includeXMark = $('#lines [name=includeXMark]').prop('checked');
 
   var newDoc = new PDFDocument({
     size: paper,
@@ -24,16 +33,15 @@ function updatePdf() {
 
   var marginAbove = 20;
   // TODO this is currently 3:2:3 for Copperplate
-  var ascender = xHeight / 2 * 3;
-  var descender = xHeight / 2 * 3;
+  var ascender = ratios[adRatio].call(null, xHeight);
+  var descender = ascender;
   var totalHeight = marginAbove + ascender + xHeight + descender;
 
   var repeatCount = Math.floor(newDoc.page.height / totalHeight);
   var leftPoint = 20;
   var rightPoint = newDoc.page.width - leftPoint;
 
-  //repeatCount = 1;
-  //console.log("repeat " + repeatCount + "left" + leftPoint + 'right' + rightPoint);
+  var xMarkBase = (totalHeight / 2) + (newDoc.currentLineHeight() / 2);
 
   for (var i = 0, offset = 0; i < repeatCount; i++, offset += totalHeight) {
     [marginAbove, ascender, xHeight, descender].reduce(function (base, additional, idx) {
@@ -47,6 +55,10 @@ function updatePdf() {
 
       return lineAt;
     }, offset);
+
+    if (includeXMark) {
+      newDoc.text('x', leftPoint, offset + xMarkBase);
+    }
   }
 
   console.log(newDoc);
